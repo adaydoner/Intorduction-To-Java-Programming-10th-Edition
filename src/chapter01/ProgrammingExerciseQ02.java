112ee306d0b0a9f1864b0f49bca41d61faf4a3cf
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-08 04:04:14 +0300

    Question 02

---
+++ src/chapter01/ProgrammingExerciseQ02.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class ProgrammingExerciseQ02 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=5 ; i++) {
+			System.out.println("Welcome to Java");
+		}
+	}
+
+}
