73f25269067f307dca3f31f6abc3d1fe752711f3
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-08 05:52:28 +0300

    Exercise 09

---
+++ src/chapter01/ProgrammingExercise09.java
@@ -0,0 +1,18 @@
+/**
+ * Chapter 1 Exercise 9:
+ *
+ *      (Area and perimeter of a rectangle) Write a program that displays the area and perimeter of a rectangle with the width of 4.5 and height of 7.9
+ */
+package chapter01;
+
+public class ProgrammingExercise09 {
+
+	public static void main(String[] args) {
+		double width = 4.5;
+		double height = 7.9;
+		
+		System.out.println("Area : " + width * height);
+
+	}
+
+}
