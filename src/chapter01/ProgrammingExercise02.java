1018c660c43e66fe6233bc07c7f55ac933ffeff8
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-08 04:07:26 +0300

    Exercise 02

---
+++ src/chapter01/ProgrammingExercise02.java
@@ -0,0 +1,12 @@
+// (Display five messages) Write a program that displays Welcome to Java five times.
+package chapter01;
+
+public class ProgrammingExercise02 {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=5 ; i++) {
+			System.out.println("Welcome to Java");
+		}
+	}
+
+}
