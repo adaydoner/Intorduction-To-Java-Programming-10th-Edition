ea42d0f0e43e4212d15df6e2344d8c36175effe8
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-08 03:58:24 +0300

    Question 01

---
+++ src/chapter01/ProgrammingExerciseQ01.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class ProgrammingExerciseQ01 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is Fun");
+
+	}
+
+}
