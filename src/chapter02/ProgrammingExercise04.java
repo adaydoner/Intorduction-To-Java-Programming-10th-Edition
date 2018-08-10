d16beee7a8810c8185cc382f1a2481948404f350
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-10 14:15:15 +0300

    Exercise 04

--- src/chapter02/ProgrammingExercise04.java
+++ src/chapter02/ProgrammingExercise04.java
@@ -1,22 +1,28 @@
+/*
+ * Chapter 2 Exercise 4:
+ *
+ *      (Convert pounds into kilograms) Write a program that converts pounds into kilo- grams.
+ *      The program prompts the user to enter a number in pounds, converts it to kilograms,
+ *      and displays the result. One pound is 0.454 kilograms.
+ */
+
 package chapter02;
 
 import java.util.Scanner;
 
 public class ProgrammingExercise04 {
-	public static double poundsIntoKilogram(double pounds) {
+	public static void poundsIntoKilogram(double pounds) {
 		double kilogram;
 		kilogram = pounds * 0.454;
-		return kilogram;
+		System.out.printf("%.2f pounds is %.3f kilograms",pounds,kilogram);
 	}
 
 	public static void main(String[] args) {
 		Scanner readNumber = new Scanner(System.in);
-		double kilogram;
 		double pounds;
 		System.out.print("Enter a number in pounds : ");
 		pounds = readNumber.nextDouble();
-		kilogram = poundsIntoKilogram(pounds);
-		System.out.printf("%.2f pounds is %.3f kilograms",pounds,kilogram);
+		poundsIntoKilogram(pounds);
 		
 		if(readNumber != null) {
 			readNumber.close();
