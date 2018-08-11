9a27773f387c03d4c15b5f79f8e484ef302dc895
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-11 23:48:21 +0300

    Exercise 09

--- src/chapter02/ProgrammingExercise09.java
+++ src/chapter02/ProgrammingExercise09.java
@@ -10,6 +10,11 @@ package chapter02;
 import java.util.Scanner;
 
 public class ProgrammingExercise09 {
+	public static double FindAcceleration(double v0 , double v1 , double t) {
+		double acceleration;
+		acceleration = (v1 - v0) / t;
+		return acceleration;
+	}
 
 	public static void main(String[] args) {
 		double v0;
@@ -22,8 +27,8 @@ public class ProgrammingExercise09 {
 		v0 = readNumber.nextDouble();
 		v1 = readNumber.nextDouble();
 		t = readNumber.nextDouble();
-		averageAcceleration = (v1 - v0) / t;
-		System.out.printf("The average acceleration is %.2f " ,averageAcceleration);
+		averageAcceleration = FindAcceleration(v0, v1, t) ;
+		System.out.printf("The average acceleration is %.3f " ,averageAcceleration);
 		
 		if(readNumber != null) {
 			readNumber.close();
