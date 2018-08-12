2146454bce8ac8be4f040e5deb93552e9db3b397
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-12 11:49:54 +0300

    wind-chill temperature by NWS's measure formulas

--- src/chapter02/ProgrammingExercise17.java
+++ src/chapter02/ProgrammingExercise17.java
@@ -11,33 +11,33 @@ import java.util.Scanner;
 
 public class ProgrammingExercise17 {
 
-
 	private static double CalculateWindChillTemperature(double outsideTemperature, double windSpeed) {
 		double windChilIndex;
-		windChilIndex = 35.74 + 0.6215 * outsideTemperature - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * outsideTemperature * Math.pow(windSpeed, 0.16);
-		windChilIndex = ((int)(windChilIndex * 100000) / 100000.0); // For get rid of lots of numbers after comma
+		windChilIndex = 35.74 + 0.6215 * outsideTemperature - 
+				35.75 * Math.pow(windSpeed, 0.16) + 
+				0.4275 * outsideTemperature * Math.pow(windSpeed, 0.16);
+		windChilIndex = ((int) (windChilIndex * 100000) / 100000.0); // For get rid of lots of numbers after comma
 		return windChilIndex;
 	}
 
 	public static void main(String[] args) {
-		double outsideTemperature;
-		double windSpeed;
+		double outsideTemperature = 42;
+		double windSpeed = 0;
 		double windChillIndex;
 		Scanner readNumber = new Scanner(System.in);
-		do {
-		System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F : ");
-		outsideTemperature = readNumber.nextDouble();
-		}while(outsideTemperature > 41 || outsideTemperature < -58);
-		
-		do {
-		System.out.print("Enter the wind speed (>=2) in miles per hour : ");
-		windSpeed = readNumber.nextDouble();
-		}while(windSpeed < 2);
-		
-		windChillIndex = CalculateWindChillTemperature(outsideTemperature,windSpeed);
+		while (outsideTemperature > 41 || outsideTemperature < -58) {
+			System.out.print("Enter the temperature in Fahrenheit between -58°F and 41°F : ");
+			outsideTemperature = readNumber.nextDouble();
+		}
+		while (windSpeed < 2) {
+			System.out.print("Enter the wind speed (>=2) in miles per hour : ");
+			windSpeed = readNumber.nextDouble();
+		}
+
+		windChillIndex = CalculateWindChillTemperature(outsideTemperature, windSpeed);
 		System.out.println("The wind chill index is " + windChillIndex);
-		
-		if(readNumber != null) {
+
+		if (readNumber != null) {
 			readNumber.close();
 		}
 	}
