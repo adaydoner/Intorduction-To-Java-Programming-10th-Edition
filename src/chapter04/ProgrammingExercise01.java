30b51a5ed67ac4654647af78d2002f01017f7205
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-18 11:49:14 +0300

    Geometry: area of a pentagon

--- src/chapter04/ProgrammingExercise01.java
+++ src/chapter04/ProgrammingExercise01.java
@@ -1,9 +1,29 @@
+/*
+ * (Geometry: area of a pentagon) Write a program that prompts the user to enter
+ * the length from the center of a pentagon to a vertex and computes the area of the
+ * pentagon, as shown in the following figure.
+ */
+
 package chapter04;
+
+import java.util.Scanner;
 
 public class ProgrammingExercise01 {
 
-	public static void main(String[] args) {
-		System.out.println("...");
+	private static void AreaOfPentagon(double r) {
+		double area;
+		double lengthOfSide;
+		lengthOfSide = 2 * r * Math.sin(Math.PI / 5);
+		area = (5 * Math.pow(lengthOfSide, 2)) / (4 * Math.tan(Math.PI / 5));
+		System.out.printf("The area of the pentagon is : %.2f ",area);
 	}
 
+	public static void main(String[] args) {
+		Scanner readNumber = new Scanner(System.in);
+		double r;
+		System.out.print("Enter the length from the center to a vertex : ");
+		r = readNumber.nextDouble();
+		AreaOfPentagon(r);
+		readNumber.close();
+	}
 }
