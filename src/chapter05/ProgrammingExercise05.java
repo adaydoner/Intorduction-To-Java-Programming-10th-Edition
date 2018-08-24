8875bd22e60fff5ad8f32b59a6bd3fa0ed45d482
Ali <40503683+adaydoner@users.noreply.github.com>
2018-08-24 18:56:31 +0300

    Conversion from kilograms to pounds and pounds to kilograms

--- src/chapter05/ProgrammingExercise05.java
+++ src/chapter05/ProgrammingExercise05.java
@@ -8,9 +8,9 @@ public class ProgrammingExercise05 {
 
 	public static void main(String[] args) {
 		final double KGtoPounds = 2.2;
-		System.out.printf("%-6s\t%6s  \t|\t%-9s\t%6s\n","Kilograms","Pounds","Pounds","Kilograms");
+		System.out.printf("%-10s%10s\t|\t%-10s%10s\n","Kilograms","Pounds","Pounds","Kilograms");
 		for (int i = 1, j = 20; i < 200; i += 2 , j += 5) {
-			System.out.printf("%-6d\t\t%6.1f  \t|\t%-6d\t\t   %6.2f\n",i, i*KGtoPounds,j,(j / KGtoPounds));
+			System.out.printf("%-10d%10.1f\t|\t%-10d%10.2f\n",i, i*KGtoPounds,j,(j / KGtoPounds));
 		}
 	}
 }
