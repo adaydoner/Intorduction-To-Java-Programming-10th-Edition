826328d96e852f212818fbfcbb0be7b48608f024
Ali <40503683+adaydoner@users.noreply.github.com>
2018-09-16 08:16:36 +0300

    Display current date and time

--- src/chapter06/ProgrammingExercise24.java
+++ src/chapter06/ProgrammingExercise24.java
@@ -14,21 +14,30 @@ public class ProgrammingExercise24 {
 		long currentSecond = totalSeconds % 60;
 		long totalMinutes = totalSeconds / 60;
 		long currentMinute = totalMinutes % 60;
-		long totalHours = totalMinutes / 60;
+		long totalHours = (totalMinutes / 60) + 3;// GMT (+3)
 		long currentHour = totalHours % 24;
-		long totalDays = totalHours / 24;
-		int currentYear = (int) (totalDays / 365) + 1970;
+		long totalDaysSince1970 = totalHours / 24;
+		int currentYear = (int) (totalDaysSince1970 / 365) + 1970; // **DD/MM//(YYYY)**
 
-		long daysInCurrentYear = (totalDays - numberOfLeapYearsSince1970(currentYear)) % 365;
+		/* if all years have 365 days we can easily do (totalDays % 365) and find daysInCurrentYear. 
+		 * Since leap years have 366 days we will subtract the NUMBER of leap years from total days so its like all years have 365 days.
+		*/
+		long daysInCurrentYear = (totalDaysSince1970 - numberOfLeapYearsSince1970(currentYear)) % 365; 
 		if (currentHour > 0) {
 			daysInCurrentYear++; // add today
 		}
-		int currentMonthNum = getMonthFromDays(currentYear, (int) daysInCurrentYear);
-		String month = getMonthName(currentMonthNum);
-
+		
+		/* Now we will find what month we'r in. When we compute this we have to consider every month has different total day.
+		 * we have how many days pass for this year , we know what month has how many days. -->> getMonthFromDays
+		 */
+		int currentMonthNum = getMonthFromDays(currentYear, (int) daysInCurrentYear); 
+		String month = getMonthName(currentMonthNum); // **DD/(MM)//YYYY**
+		/*
+		 * Now we will find what days in current month we are at. we have how many day passed untill current month in current year. we have total days in current year.
+		 * when we subtract them we have today.
+		 */
 		int daysTillCurrentMonth = getNumOfDaysToReachThatMonth(currentYear, currentMonthNum);
-
-		int today = (int) daysInCurrentYear - daysTillCurrentMonth;
+		int today = (int) daysInCurrentYear - daysTillCurrentMonth; // **(DD)/MM//YYYY**
 
 		return (today + "/" + month + "/"+ currentYear + "\n" + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT");
 	}
@@ -44,16 +53,14 @@ public class ProgrammingExercise24 {
 	}
 
 	public static int getMonthFromDays(int year, int days) {
-		int dayTracker = 0;
-		for (int i = 1; i <= 12; i++) {
-
-			dayTracker += getNumberOfDaysInMonth(year, i);
-
-			if (dayTracker > days)
-				return i;
+		int dayCounter = 0; // we count days untill daysInCurrentYear value.
+		int monthNumber = 0;
+		while(dayCounter < days) {
+			monthNumber++;
+			dayCounter += getNumberOfDaysInMonth(year, monthNumber); // we consider that every month has different total days. 
+			}
+		return monthNumber;
 		}
-		return 12;
-	}
 
 	public static int getStartDay(int year, int month) {
 		final int START_DAY_FOR_JAN_1_1800 = 3;
@@ -83,16 +90,14 @@ public class ProgrammingExercise24 {
 
 	public static int getNumberOfDaysInMonth(int year, int month) {
 
-		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
 			return 31;
-
-		if (month == 4 || month == 6 || month == 9 || month == 11)
-			return 30;
-
-		if (month == 2)
+		}
+		if (month == 2) {
 			return isLeapYear(year) ? 29 : 28;
-
-		return 0; // If month is incorrect
+		} else {
+			return 30;
+		}
 	}
 
 	public static int getNumOfDaysToReachThatMonth(int year, int month) {
