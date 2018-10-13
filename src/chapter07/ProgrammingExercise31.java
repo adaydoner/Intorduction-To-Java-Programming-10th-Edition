b7f23ee69782a4fc6e4b5645cac4f9ffc65aab04
Ali <40503683+adaydoner@users.noreply.github.com>
2018-10-13 18:15:50 +0300

    Merge two sorted lists

--- src/chapter07/ProgrammingExercise31.java
+++ src/chapter07/ProgrammingExercise31.java
@@ -47,37 +47,40 @@ public class ProgrammingExercise31 {
 
 	public static int[] merge(int[] list1, int[] list2) {
 		int[] mergedList = new int[list1.length + list2.length];
+		// two counter initialization to get lists element individually and mergedList index holder.
 		int list1Counter = 0;
 		int list2Counter = 0;
 		int lastWrittenIndex = 0;
+		// two boolean initialization for using to detect which list is smaller so we can print the larger list's element after loop. 
 		boolean list1Smaller = false;
 		boolean list2Smaller = false;
+		
 		for (int writer = 0; writer < mergedList.length; writer++) {
 			lastWrittenIndex = writer;
-			if (list1[list1Counter] <= list2[list2Counter]) {
-				mergedList[writer] = list1[list1Counter];
+			if (list1[list1Counter] <= list2[list2Counter]) { // if its true we get next list1 element to our mergedlist
+				mergedList[writer] = list1[list1Counter]; 
 				list1Counter++;
 				if(list1Counter == list1.length) { // if next step would be give an ArrayIndexOutOfBoundsException
-					list1Smaller = true;
+					list1Smaller = true; // set list1smaller flag true and break the loop.
 					break;
 				}
-			} else {
+			} else { // else we get next list2 element to our mergedList
 				mergedList[writer] = list2[list2Counter];
 				list2Counter++;
 				if(list2Counter == list2.length) { // if next step would be give an ArrayIndexOutOfBoundsException
-					list2Smaller = true;
+					list2Smaller = true; // set list2smaller flag true and break the loop.
 					break;
 				}
 			}
 		}
 		
-		if(list1Smaller) {
-			for (int i = list2Counter; i < list2.length; i++,lastWrittenIndex++) {
+		if(list1Smaller) { // if list1 is smaller , we will print rest of LIST2 element to our mergedlist , since lists supposed to be sorted no need to check any element.
+			for (int i = list2Counter; i < list2.length; i++,lastWrittenIndex++) { // from list2Counter which is index of list2 that doesn't written to list2's last element. 
 				mergedList[lastWrittenIndex] = list2[i];
 			}
-		} else if(list2Smaller) {
+		} else if(list2Smaller) { //  we will print rest of LIST1 element to our mergedlist , since lists supposed to be sorted no need to check any element.
 			for (int i = list1Counter; i < list1.length; i++,lastWrittenIndex++) {
-				mergedList[lastWrittenIndex + 1] = list1[i];
+				mergedList[lastWrittenIndex + 1] = list1[i]; // from list1Counter which is index of list1 that doesn't written to list1's last element.
 			}
 		}
 
