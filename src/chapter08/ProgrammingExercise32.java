f967168a17cb70fc85c9f2b8bb8f949fd92b6617
Ali <40503683+adaydoner@users.noreply.github.com>
2018-10-27 17:31:37 +0300

    Geometry: area of a triangle

--- src/chapter08/ProgrammingExercise32.java
+++ src/chapter08/ProgrammingExercise32.java
@@ -35,21 +35,8 @@ public class ProgrammingExercise32 {
 	
 	
 	public static double getTriangleArea(double[][] points) {
-		double side1 = FindDistanceOfPoints(points[0][0], points[0][1], points[1][0], points[1][1]);
-		double side2 = FindDistanceOfPoints(points[0][0], points[0][1], points[2][0], points[2][1]);
-		double side3 = FindDistanceOfPoints(points[2][0], points[2][1], points[1][0], points[1][1]);
-		double s = (side1 + side2 + side3) / 2;
-		double area = s * (s - side1) * (s - side2) * (s - side3);
-
-		if (area < 0.000001)
-			return 0;
-		else
-			return Math.sqrt(area);
-	}
-
-	private static double FindDistanceOfPoints(double x1, double y1, double x2, double y2) {
-		double calculation;
-		calculation = Math.pow((Math.pow(x2 - x1, 2)) + Math.pow(y2 - y1, 2), 0.5);
-		return calculation;
+		double area = 0;
+		area = (points[0][0] * (points[1][1] - points[2][1] ) + points[1][0] * (points[2][1] - points[0][1]) + points[2][0] * (points[0][1] - points[1][1])) / 2;
+		return area;
 	}
 }
