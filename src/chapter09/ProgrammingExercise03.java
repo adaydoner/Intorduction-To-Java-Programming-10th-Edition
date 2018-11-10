a9d68e3f542bd3095c8d9c996d7a034168578a4c
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-11-10 15:07:32 +0300

    Use the Date class

--- src/chapter09/ProgrammingExercise03.java
+++ src/chapter09/ProgrammingExercise03.java
@@ -1,5 +1,5 @@
 /*
- * (Use the Date class) Write a program that creates a Date object, 
+ * (Use the Date class ) Write a program that creates a Date object, 
  * sets its elapsed time to 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, and 100000000000, and displays the date and time using the toString() method, respectively.
  */
 package chapter09;
