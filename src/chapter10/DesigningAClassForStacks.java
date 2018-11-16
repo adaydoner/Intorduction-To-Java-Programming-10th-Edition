159741c6a257f100f85165a3f58e95bdd515c7a0
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-11-16 23:13:16 +0300

    case study designing a class for Stacks

--- src/chapter10/DesigningAClassForStacks.java
+++ src/chapter10/DesigningAClassForStacks.java
@@ -6,7 +6,7 @@ public class DesigningAClassForStacks {
 	public static void main(String[] args) {
 		StackOfIntegers stack = new StackOfIntegers();
 
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 20; i++) {
 			stack.push(i);
 		}
 
@@ -18,31 +18,32 @@ public class DesigningAClassForStacks {
 }
 
 class StackOfIntegers {
-	//An array to store integers in the stack.
+	// An array to store integers in the stack.
 	private int[] elements;
-	//The number of integers in the stack.
+	// The number of integers in the stack.
 	private int size;
-	
-	//Constructs an empty stack with a default capacity of 16.
+
+	// Constructs an empty stack with a default capacity of 16.
 	public StackOfIntegers() {
 		elements = new int[16];
 	}
-	
-	//Constructs an empty stack with a specified capacity.
+
+	// Constructs an empty stack with a specified capacity.
 	public StackOfIntegers(int capacity) {
 		elements = new int[capacity];
 	}
-	
-	//Returns true if the stack is empty.
+
+	// Returns true if the stack is empty.
 	public boolean empty() {
-		if(size == 0){
+		if (size == 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	//Returns the integer at the top of the stack without removing it from the stack.
+	// Returns the integer at the top of the stack without removing it from the
+	// stack.
 	public int peek() {
 		if (size == 0) {
 			return 0;
@@ -51,13 +52,21 @@ class StackOfIntegers {
 		}
 	}
 
-	//Stores an integer into the top of the stack.
+	// Stores an integer into the top of the stack.
 	public void push(int value) {
-		elements[size] = value;
-		size++;
+		if (size >= elements.length) {
+			int[] temp = new int[elements.length + 1];
+			System.arraycopy(elements, 0, temp, 0, elements.length);
+			elements = temp;
+			elements[size] = value;
+			size++;
+		} else {
+			elements[size] = value;
+			size++;
+		}
 	}
 
-	//Removes the integer at the top of the stack and returns it.
+	// Removes the integer at the top of the stack and returns it.
 	public int pop() {
 		if (size == 0) {
 			return 0;
@@ -69,8 +78,8 @@ class StackOfIntegers {
 			return returnVal;
 		}
 	}
-	
-	//Returns the number of elements in the stack.
+
+	// Returns the number of elements in the stack.
 	public int getSize() {
 		return size;
 	}
