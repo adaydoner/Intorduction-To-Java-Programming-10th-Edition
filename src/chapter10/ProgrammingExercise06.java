1bdabf517838ebf701fee1609f45d5c80e627733
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-11-20 22:56:18 +0300

    Displaying the prime numbers

--- src/chapter10/ProgrammingExercise06.java
+++ src/chapter10/ProgrammingExercise06.java
@@ -1,4 +1,5 @@
 package chapter10;
+import Classes.MyInteger;
 /**
  * (Displaying the prime numbers) Write a program that displays all the prime numbers less than 120 in decreasing order. 
  * Use the StackOfIntegers class to store the prime numbers (e.g., 2, 3, 5, ...) and retrieve and display them in reverse order.
