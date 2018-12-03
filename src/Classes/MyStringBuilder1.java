048924a10f4821c0e549e4fe9b004af1e2f6fdea
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-12-03 22:43:47 +0300

    Implement the StringBuilder class

--- src/Classes/MyStringBuilder1.java
+++ src/Classes/MyStringBuilder1.java
@@ -7,6 +7,10 @@ package Classes;
 public class MyStringBuilder1 {
 	private char[] val;
 	
+	/**
+	 * Create a new object from specified String s value.
+	 * @param s
+	 */
 	public MyStringBuilder1 (String s){
 		val = new char[s.length()];
 		for (int i = 0; i < s.length(); i++) {
@@ -14,31 +18,80 @@ public class MyStringBuilder1 {
 		}
 	}
 	
-	
+	/**
+	 * this method return a new MyStringBuilder1 object with the value of (instance value + specified objects value).
+	 * instance value : value of the object that called this method.
+	 * @param s
+	 * @return
+	 */
 	public MyStringBuilder1 append(MyStringBuilder1 s){
-		char[] val2 = new char[val.length + s.val.length];
-		copyArrays(val,val2,0,0,val.length);
-		copyArrays(s.val,val2,0,val.length,s.val.length);
-		
-		return new MyStringBuilder1(String.valueOf(val2));
+		return new MyStringBuilder1(this.toString() + s.toString());
 	}
 
-	private void copyArrays(char[] source, char[] dest, int sourcePos,int destPos, int length) {
-		for (int i = sourcePos,j = destPos; i < length; i++,j++) {
-			dest[j] = source[i];
-		}
+	/**
+	 * this method return a new MyStringBuilder1 object with the value of (instance value + specified integer value).
+	 * instance value : value of the object that called this method.
+	 * @param i
+	 * @return
+	 */
+	public MyStringBuilder1 append(int i) {
+		return append(new MyStringBuilder1(String.valueOf(i)));
 	}
 	
-	
+	/**
+	 * this method return length of this object's value
+	 * @return
+	 */
+	public int length() {
+		return val.length;
+	}
 	
 	/**
-	 * getters and setters
+	 * this method return a char of this object's specified index
+	 * @param index
+	 * @return
 	 */
-	public String getVal(){
-		String returnVal = "";
-		for (int i = 0; i < val.length; i++) {
-			returnVal += val[i];
+	public char charAt(int index){
+		return val[index];
+	}
+	
+	/**
+	 * Converts all of the characters in this String to lower case using the rules of the default locale.
+	 * @return
+	 */
+	public MyStringBuilder1 toLowerCase(){
+		char[] returnVal = new char[val.length];
+		for (int i = 0; i < returnVal.length; i++) {
+			if('A' <= val[i] && val[i] <= 'Z'){
+				returnVal[i] = (char)(val[i] - 'A' + 'a');
+			} else {
+				returnVal[i] = val[i];
+			}
 		}
-		return returnVal;
+		return new MyStringBuilder1(String.valueOf(returnVal));
+	}
+	
+	/**
+	 * this method return a new MyStringBuilder1 object from this objects specified indexes
+	 * begin : this index value would be the start of new objects
+	 * end   : this index value would be the end of new objects
+	 * @param begin
+	 * @param end
+	 * @return
+	 */
+	public MyStringBuilder1 substring(int begin, int end){
+		char[] returnVal = new char[val.length];
+		for (int i = begin,writer = 0; i < end; i++,writer++) {
+			returnVal[writer] = val[i];
+		}
+		
+		return new MyStringBuilder1(String.valueOf(returnVal));
+	}
+	
+	/**
+	 * return a String of this objects value. 
+	 */
+	public String toString(){
+		return String.valueOf(val);
 	}
 }
--- src/chapter10/ProgrammingExercise27.java
+++ src/chapter10/ProgrammingExercise27.java
@@ -20,6 +20,15 @@ import Classes.MyStringBuilder1;
 public class ProgrammingExercise27 {
 	public static void main(String[] args) {
 		MyStringBuilder1 msb = new MyStringBuilder1("ali");
-		System.out.println(msb.append(new MyStringBuilder1("sonraki")).getVal());
+		MyStringBuilder1 msb2 = new MyStringBuilder1("KARENINA");
+		System.out.println("Object 1 : " + msb.toString());
+		System.out.println("Object 2 : " + msb2.toString());
+		System.out.println("Append method for ali + next : " + msb.append(new MyStringBuilder1("next")).toString());
+		System.out.println("Append method for KARENINA + integerNum(1234) : " + msb2.append(1234).toString());
+		System.out.println("length method for ali : " + msb.length());
+		System.out.println("length method for KARENINA : " + msb2.length());
+		System.out.println("to lower case method for KARENINA : " + msb2.toLowerCase().toString());
+		System.out.println("substring method for KARENINA(2-5) : " + msb2.substring(2, 5).toString());
+		
 	}
 }
