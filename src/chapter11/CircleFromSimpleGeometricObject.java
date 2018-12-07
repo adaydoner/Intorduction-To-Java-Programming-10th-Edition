35b969192e4436d543d2daa9bd3c6237b16110d2
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-12-07 11:46:42 +0300

    test

---
+++ src/chapter11/CircleFromSimpleGeometricObject.java
@@ -0,0 +1,39 @@
+package chapter11;
+import chapter11.SimpleGeometricObject;
+
+public class CircleFromSimpleGeometricObject extends SimpleGeometricObject {
+	private double radius;
+	
+	CircleFromSimpleGeometricObject(double radius){
+		this(radius,"white");
+	}
+	
+	CircleFromSimpleGeometricObject(double radius, String color){
+		this(radius,color,false);
+	}
+	
+	CircleFromSimpleGeometricObject(double radius, String color, boolean filled){
+		this.radius = radius;
+		setColor(color);
+		setFilled(filled);
+	}
+	public double getArea(){
+		return Math.PI * radius * radius;
+	}
+	public double getPerimeter(){
+		return 2 * Math.PI * radius;
+	}
+	public double getDiameter(){
+		return 2 * radius;
+	}
+	
+	/**
+	 * getters and setters
+	 */
+	public double getRadius(){
+		return radius;
+	}
+	public void setRadius(double radius){
+		this.radius = radius;
+	}
+}
