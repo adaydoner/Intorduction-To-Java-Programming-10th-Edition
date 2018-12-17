e0e7b948b6afe06f94f3b18e385690d02074706b
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2018-12-17 23:01:59 +0300

    The Triangle class

---
+++ src/Classes/Triangle.java
@@ -0,0 +1,70 @@
+package Classes;
+/**
+ * initialized with conditions that giving at Chapter11.PE.01
+ * @author Ali Dogan Aydoner
+ *
+ */
+public class Triangle extends GeometricObject {
+	private double side1;
+	private double side2;
+	private double side3;
+	
+	public Triangle() {
+		this(1,1,1);
+	}
+	
+	/**
+	 * this will set triangle's side length from given values.
+	 * if values cant make a valid triangle , new object would initialize with;
+	 * side1 = 1
+	 * side2 = 1 
+	 * side3 = 1
+	 * @param side1
+	 * @param side2
+	 * @param side3
+	 */
+	public Triangle (double side1, double side2, double side3) {
+		if(isTriangle(side1,side2,side3)){
+			this.side1 = side1;
+			this.side2 = side2;
+			this.side3 = side3;
+		} else {
+			this.side1 = 1;
+			this.side2 = 1;
+			this.side3 = 1;
+		}
+	}
+	
+	private boolean isTriangle(double side1, double side2, double side3) {
+		if(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1){
+			return true;
+		}
+		return false;
+	}
+
+	public double getArea() {
+		double p = getPerimeter() / 2;
+		return Math.abs(Math.sqrt(p * (p - side1) * (p - side2) * (p - side3)));
+	}
+	
+	public double getPerimeter() {
+		return side1 + side2 + side3;
+	}
+	
+	@Override
+	public String toString() {
+		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+	}
+	/*
+	 * getters and setters
+	 */
+	public double getSide1() {
+		return side1;
+	}
+	public double getSide2() {
+		return side2;
+	}
+	public double getSide3() {
+		return side3;
+	}
+}
---
+++ src/chapter11/ProgrammingExercise01.java
@@ -0,0 +1,51 @@
+package chapter11;
+import java.util.Scanner;
+import Classes.GeometricObject;
+import Classes.Triangle;
+/**
+ * (The Triangle class) Design a class named Triangle that extends GeometricObject. The class contains:
+ *  Three double data fields named side1, side2, and side3 with default values 1.0 to denote three sides of the triangle.
+ * A no-arg constructor that creates a default triangle.
+ * A constructor that creates a triangle with the specified side1, side2, and side3.
+ * The accessor methods for all three data fields.
+ * A method named getArea() that returns the area of this triangle.
+ * A method named getPerimeter() that returns the perimeter of this triangle.
+ * A method named toString() that returns a string description for the triangle.
+ * 
+ * For the formula to compute the area of a triangle, see Programming Exercise 2.19. The toString() method is implemented as follows:
+ * 		return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+ * Draw the UML diagrams for the classes Triangle and GeometricObject and
+ * implement the classes. Write a test program that prompts the user to enter three
+ * sides of the triangle, a color, and a Boolean value to indicate whether the triangle
+ * is filled. The program should create a Triangle object with these sides and set
+ * the color and filled properties using the input. The program should display
+ * the area, perimeter, color, and true or false to indicate whether it is filled or not
+ */
+
+public class ProgrammingExercise01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double[] triangleSideLengths = new double[3];
+		String color;
+		boolean filled;
+		System.out.print("Enter three side length to make a triangle : ");
+		for (int i = 0; i < triangleSideLengths.length; i++) {
+			triangleSideLengths[i] = input.nextDouble();
+		}
+		System.out.print("Color ?  isFilled ?(true or false) : ");
+		color = input.nextLine();
+		filled = input.nextBoolean();
+		
+		Triangle t1 = new Triangle(triangleSideLengths[0],triangleSideLengths[1],triangleSideLengths[2]);
+		t1.setColor(color);
+		t1.setFilled(filled);
+		
+		System.out.print("Area of triangle :" + t1.getArea() + "\n");
+		System.out.print("Perimeter of triangle" + t1.getPerimeter() + "\n");
+		System.out.print("Color : " + t1.getColor() + "\n");
+		System.out.print("Filled : " + t1.isFilled() + "\n");
+		
+		
+		input.close();
+	}
+}
